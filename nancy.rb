bbd57f1a2cb2dc02df2c1ecac1a8b62a178bf225
xortiz <xavier.ortiz.ch@gmail.com>
2017-05-28 17:56:46 -0400

    able to get nancy.routes by running ruby.

---
+++ nancy.rb
@@ -0,0 +1,41 @@
+# nancy.rb
+require "rack"
+
+module Nancy
+  class Base
+    def initialize
+      @routes = {}
+    end
+
+    attr_reader :routes
+
+    def get(path, &handler)
+      route("GET", path, &handler)
+    end
+
+    private
+
+    def route(verb, path, &handler)
+      @routes[verb] ||= {}
+      @routes[verb][path] = handler
+    end
+  end
+
+  def call(env)
+    @request = Rack::Request.new(env)
+    verb = @request.request_method
+    requested_path = @request.path_info
+
+    handler = @routes[verb][requested_path]
+
+    handler.call
+  end
+
+end
+nancy = Nancy::Base.new
+
+nancy.get "/hello" do
+  [200, {}, ["Nancy says hello"]]
+end
+
+puts nancy.routes
