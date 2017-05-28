4118159ceb933611e18ce69e672835877a20dad4
xortiz <xavier.ortiz.ch@gmail.com>
2017-05-28 18:17:02 -0400

    added some functionality as a Rack app for localhost/hello, as well as some error handling.

--- nancy.rb
+++ nancy.rb
@@ -13,24 +13,25 @@ module Nancy
       route("GET", path, &handler)
     end
 
-    private
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+      handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
 
+      if handler
+        handler.call
+      else
+        [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
+      end
+    end
+
+    private
     def route(verb, path, &handler)
       @routes[verb] ||= {}
       @routes[verb][path] = handler
     end
   end
-
-  def call(env)
-    @request = Rack::Request.new(env)
-    verb = @request.request_method
-    requested_path = @request.path_info
-
-    handler = @routes[verb][requested_path]
-
-    handler.call
-  end
-
 end
 nancy = Nancy::Base.new
 
@@ -38,4 +39,4 @@ nancy.get "/hello" do
   [200, {}, ["Nancy says hello"]]
 end
 
-puts nancy.routes
+Rack::Handler::WEBrick.run nancy, Port: 9292
